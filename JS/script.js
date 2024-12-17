083efa65fdb107e5872d1c1799d2075ec018d7b5
rgalarza <130817814+kovac77@users.noreply.github.com>
2024-12-17 01:00:25 -0300

    actializaCarrito() y eliminarProducto()

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,5 @@
 
-
+//-------------PRODUCTOS----------------
 /*lista de productos*/
 let productos = [
     {
@@ -7,6 +7,7 @@ let productos = [
         "nombre": "Titulo 1",
         "precio": 12.50,
         "descripcion": "Descripcion 1",
+        "cantidad": 0,
         "urlImagen": "./imagenes/galeria/tanjiro.png"
     },
     {
@@ -14,6 +15,7 @@ let productos = [
         "nombre": "Titulo 2",
         "precio": 12.50,
         "descripcion": "Descripcion 2",
+        "cantidad": 0,
         "urlImagen": "./imagenes/galeria/tanjiro.png",
     },
 
@@ -37,8 +39,8 @@ document.addEventListener("DOMContentLoaded", (event) => {
                                 <img src="${producto.urlImagen}" class="img-tarjeta"
                                     alt="imagen-producto">
                                 <div class="link-tarjeta">
-                                    <a href="#" onclick="agregarAlCarrito('${producto.id}')><i class="fa-regular fa-eye"></i></a>
-                                    <a href="carrito.html"><i class="fa-solid fa-cart-plus"></i></a>
+                                    <a href="carrito.html"><i class="fa-regular fa-eye"></i></a>
+                                    <a href="#" onclick="agregarAlCarrito('${producto.id}')"><i class="fa-solid fa-cart-plus"></i></a>
                                     <a href="carrito.html"><i class="fa-solid fa-heart"></i></a>
                                 </div>
                             </div>`;
@@ -47,29 +49,61 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
 });
 
-function agregarAlCarrito(id){
+//----------CARRITO-----------------------
+function agregarAlCarrito(id) {
     //obtener elemento por su id
-    const idProducto=document.getElementById(id);
+    const idProducto = document.getElementById(id);
     //atributos del producto
-    const nombreProducto= idProducto.getAttribute('nombre');
-    const precioProducto= idProducto.getAttribute('precio');
-    const descripcionProducto= idProducto.getAttribute('descripcion');
-    const imagenProducto= idProducto.getAttribute('imagen');
+    const nombreProducto = idProducto.getAttribute('nombre');
+    const precioProducto = idProducto.getAttribute('precio');
+    const descripcionProducto = idProducto.getAttribute('descripcion');
+    const imagenProducto = idProducto.getAttribute('imagen');
 
     //Obatener carrito del LocalStorage o vacio y asignarselo a arregloCarrito
-    let arregloCarrito= JSON.parse(localStorage.getItem('carrito')) || [];
-    //agregar el producto al arregloCarrito
-    arregloCarrito.push({id: id, nombreProducto, precioProducto, descripcionProducto, imagenProducto});
+    let arregloCarrito = JSON.parse(localStorage.getItem('carrito')) || [];
+
+
+    let productoExistente = arregloCarrito.find(producto => producto.id === id);
+    if (productoExistente) {
+        //si existe suma cantidad
+        idProducto.cantidad++;
+    } else {
+        //agregar el producto al arregloCarrito con cantidad 1
+        arregloCarrito.push({ id: id, nombreProducto, precioProducto, descripcionProducto, imagenProducto, cantidad: 1 });
+
+    }
     //Guardar carrito en LocalStorage
     localStorage.setItem('carrito', JSON.stringify(arregloCarrito))
+    actualizarCarrito();
+}
+
+function eliminarProducto(id) {
+    let arregloCarrito = JSON.parse(localStorage.getItem('carrito')) || [];
+    arregloCarrito = arregloCarrito.filter(function (producto) {
+        return producto.id !== id;
+    });
+    localStorage.setItem('carrito', JSON.stringify(arregloCarrito));
+    actualizarCarrito();
 }
 
 
-// function actualizarCarrito(){
-//     let arregloCarrito= JSON.parse(localStorage.getItem("carrito")) || [];
-//     let contenedorProductos= document.getElementById("contenedor-producto1");
-//     contenedorProductos.innerHTML="";
-//     arregloCarrito.forEach((producto) =>{
+function actualizarCarrito() {
+    let arregloCarrito = JSON.parse(localStorage.getItem('carrito')) || [];
+    let contenedorCarrito = document.getElementById('contenedor-carrito');
 
-//     });
-// }
+    contenedorCarrito.innerHTML = "";
+    arregloCarrito.forEach((producto) => {
+        let li = document.createElement("li");
+        li.innerHTML = `
+         ${producto.nombreProducto} - $ ${parseFloat(producto.precioProducto).toFixed(2)} x ${producto.cantidad}
+        <button onclick="eliminarProducto(${producto.id})">Eliminar </button>
+         `;
+
+        contenedorCarrito.appendChild(li);
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    actualizarCarrito();
+});
+
